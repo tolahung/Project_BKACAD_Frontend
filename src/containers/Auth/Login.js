99dd3440e3dd0848b86a2632e9cc6a577537525c
tolahung <idoky19@gmail.com>
2024-02-06 11:17:34 +0700

    done 36

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -4,7 +4,8 @@ import { push } from "connected-react-router";
 
 import * as actions from "../../store/actions";
 import './Login.scss';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
+import  { handleLoginApi }  from '../../services/userService';
 
 
 
@@ -12,6 +13,65 @@ import { FormattedMessage } from 'react-intl';
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isShowPass:false,
+            errMessage: ''
+        }
+    }
+
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+        console.log(event.target.value);
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+        console.log(event.target.value);
+    }
+
+
+    // Login 
+    handleLogin = async () => {
+        this.setState({
+            errMessage: ''
+        })
+
+        try{
+            let data = await handleLoginApi(this.state.username, this.state.password);
+            if(data && data.errCode!== 0){
+                this.setState({
+                    errMessage: data.message
+                })
+            }
+            if(data && data.errCode === 0){
+                // userLoginSuccess(data.user);
+                this.props.userLoginSuccess(data.user);
+                console.log('log in success');
+            }
+        }catch(error){
+            if(error.response){
+                if(error.response.data){
+                    this.setState({
+                        errMessage: error.response.data.message
+                    })
+                }
+            }
+            console.log('hoidanit', error.response);
+        
+        }
+    }
+
+    //show passw
+    handleShowPass = ()=>{
+       this.setState({
+        isShowPass: !this.state.isShowPass
+       })
     }
 
 
@@ -24,14 +84,23 @@ class Login extends Component {
                         <div className='col-12 text-login'>Login</div>
                         <div className='col-12 form-group login-input'>
                             <label>Username:</label>
-                            <input type='text' className='form-control' placeholder='Enter your username' />
+                            <input type='text' value={this.state.username} className='form-control' placeholder='Enter your username' onChange={(e) => this.handleOnChangeUsername(e)} />
                         </div>
                         <div className='col-12 form-group login-input'>
                             <label>password:</label>
-                            <input type='password' className='form-control' placeholder='Enter your password' />
+                            <div className='custom-input-password'>
+                                <input type={this.state.isShowPass ? 'text' : 'password'} value={this.state.password} className='form-control' placeholder='Enter your password' onChange={(e) => { this.handleOnChangePassword(e) }} />
+                                <span onClick = {() => { this.handleShowPass() }}>
+                                    <i class= {this.state.isShowPass ? 'fas fa-eye': 'fas fa-eye-slash'}></i>
+                                </span>
+                            </div>
+
+                        </div>
+                        <div className = "col-12" style={{color: 'red'}}>
+                            {this.state.errMessage}
                         </div>
                         <div className='col-12'>
-                            <button className='btn-login'>Login</button>
+                            <button className='btn-login' onClick={() => { this.handleLogin() }}>Login</button>
                         </div>
                         <div className='col-12'>
                             <span className='forgot-password'>Forgot your password</span>
@@ -44,7 +113,7 @@ class Login extends Component {
                             <i className='fab fa-facebook-f facebook' ></i>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         )
     }
@@ -59,8 +128,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.adminLoginFail()),
+        userLoginSuccess: (userInfo) => dispatch(actions.userLoginSuccess(userInfo))
     };
 };
 
---
+++ src/services/userService.js
@@ -0,0 +1,6 @@
+
+import axios from "../axios";
+const handleLoginApi =  (email, password) => {
+   return   axios.post('/api/login',{email, password});
+}
+export {handleLoginApi} 
